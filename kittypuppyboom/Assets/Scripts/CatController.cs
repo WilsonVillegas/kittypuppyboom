c08b6ddec848ca58273a9c2f25e6ca0472c15ba7
Cody Sivek <cody.sivek@yahoo.com>
2014-09-27 00:10:43 -0500

    Uploading various c# files for use and review.

---
+++ kittypuppyboom/Assets/Scripts/CatController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatController : MonoBehaviour {
+	
+	public float jumpHeight;
+	public float moveSpeed;
+	public Transform groundCheck;
+	public LayerMask groundLayer;
+
+	private Vector3 moveVector;
+	private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Physics.OverlapSphere(groundCheck.position,.1f,groundLayer).Length != 0)
+		{
+			grounded = true;
+		}
+		else
+			grounded = false;
+	
+		if(Input.GetButton("Left"))
+		{
+			rigidbody.velocity = new Vector3(moveSpeed,rigidbody.velocity.y,0);
+		}
+
+		if(grounded && Input.GetButtonDown("CatJump"))
+		{
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x,jumpHeight,0);
+			grounded = false;
+		}
+	}
+
+	public void changeJump(float changeAmt)
+	{
+		jumpHeight += changeAmt;
+	}
+
+	public void changeSpeed(float changeAmt)
+	{
+		moveSpeed += changeAmt;
+	}
+
+}
---
+++ kittypuppyboom/Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public CatController cat;
+	public DogController dog;
+	public GameObject[] catPlatforms;
+	public GameObject[] dogPlatforms;
+
+	public float speedAmount;
+	public float slowAmount;
+	public float shrinkAmount;
+	public float waitAmount;
+
+	// Use this for initialization
+	void Start () {
+		catPlatforms = GameObject.FindGameObjectsWithTag("catPlatform");
+		dogPlatforms = GameObject.FindGameObjectsWithTag("dogPlatform");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator SpeedPowerUp(float player)
+	{
+		if(player == 0)
+		{
+			cat.changeSpeed(speedAmount);
+			yield return new WaitForSeconds(waitAmount);
+			cat.changeSpeed(-speedAmount);
+		}
+		else
+		{
+			dog.changeSpeed(speedAmount);
+			yield return new WaitForSeconds(waitAmount);
+			dog.changeSpeed(-speedAmount);
+		}
+	}
+
+	/*public IEnumerator ShrinkPowerUp(float player)
+	{
+		if(player == 0)
+		{
+
+		}
+		else
+		{
+			
+		}
+	}*/
+
+	public IEnumerator SlowPowerUp(float player)
+	{
+		if(player == 0)
+		{
+			cat.changeSpeed(slowAmount);
+			yield return new WaitForSeconds(waitAmount);
+			cat.changeSpeed(-slowAmount);
+		}
+		else
+		{
+			dog.changeSpeed(slowAmount);
+			yield return new WaitForSeconds(waitAmount);
+			dog.changeSpeed(-slowAmount);
+		}
+	}
+
+
+}
