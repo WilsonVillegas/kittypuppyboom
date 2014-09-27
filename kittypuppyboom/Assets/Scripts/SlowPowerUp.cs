6e1a3bec75daf66ddb6474ddba9dcd5ed275f4d2
rllamas <ricardo.llamas11@yahoo.com>
2014-09-27 01:50:39 -0500

    Slow/Speed power ups should theoretically work now. Theoretically.

--- kittypuppyboom/Assets/Scripts/SlowPowerUp.cs
+++ kittypuppyboom/Assets/Scripts/SlowPowerUp.cs
@@ -2,19 +2,33 @@
 using System.Collections;
 
 public class SlowPowerUp : MonoBehaviour {
-
+	private bool buff;
+	private GameController controller;
+	
 	// Use this for initialization
 	void Start () {
-	
+		controller = GameObject.Find("GameController").GetComponent<GameController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(this.tag.Equals("catPowerUp") && buff != controller.catState){
+			buff = controller.catState;
+		}else if(this.tag.Equals("dogPowerUp") && buff != controller.dogState){
+			buff = controller.dogState;
+		}
 	}
-
+	
 	void OnTriggerEnter(Collider other)
 	{
-		
+		if(this.tag.Equals("catPowerUp") && buff){
+			controller.SlowPowerUp(0);
+		}else if (this.tag.Equals("catPowerUp")){
+			controller.SlowPowerUp(1);
+		}else if (buff){
+			controller.SlowPowerUp(1);
+		} else {
+			controller.SlowPowerUp(0);
+		}
 	}
 }
--- kittypuppyboom/Assets/Scripts/SpeedPowerUp.cs
+++ kittypuppyboom/Assets/Scripts/SpeedPowerUp.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class SpeedPowerUp : MonoBehaviour {
 
 	private bool buff;
-	private Component controller;
+	private GameController controller;
 
 	// Use this for initialization
 	void Start () {
-		controller = GameObject.Find("GameController").GetComponents<GameController>();
+		controller = GameObject.Find("GameController").GetComponent<GameController>();
 	}
 	
 	// Update is called once per frame
@@ -25,11 +25,11 @@ public class SpeedPowerUp : MonoBehaviour {
 		if(this.tag.Equals("catPowerUp") && buff){
 			controller.SpeedPowerUp(0);
 		}else if (this.tag.Equals("catPowerUp")){
-			controller.SlowPowerUp(0);
+			controller.SpeedPowerUp(1);
 		}else if (buff){
 			controller.SpeedPowerUp(1);
 		} else {
-			controller.SlowPowerUp(1);
+			controller.SpeedPowerUp(0);
 		}
 	}
 }
