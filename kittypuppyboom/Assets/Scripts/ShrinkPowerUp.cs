f2f346a32fa6a9a269237803ccac9457ce1f22dd
Cody Sivek <cody.sivek@yahoo.com>
2014-09-27 15:10:33 -0500

    Got coin animations working.

--- kittypuppyboom/Assets/Scripts/ShrinkPowerUp.cs
+++ kittypuppyboom/Assets/Scripts/ShrinkPowerUp.cs
@@ -5,10 +5,12 @@ public class ShrinkPowerUp : MonoBehaviour {
 
 	private GameController controller;
 	public bool state; //0 = cat, 1 = dog
+	Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		controller = GameObject.Find("GameController").GetComponent<GameController>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -16,10 +18,12 @@ public class ShrinkPowerUp : MonoBehaviour {
 	 if(tag.Equals("CatPowerUp"))
 		{
 			state = !controller.catState; //True catstate = 1, so we need to change it to 0 so it applies to cats
+			animator.SetBool("State",state);
 		}
 		else
 		{
 			state = controller.dogState; //True dog state = 1, which is correct.
+			animator.SetBool("State",state);
 		}
 	}
 
