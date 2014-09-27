46840648914d748e7f74a767cb11b6f26cf1eb24
rllamas <ricardo.llamas11@yahoo.com>
2014-09-27 01:01:03 -0500

    I wrote pretty code in SpeedPowerUp.cs. I think it works.

--- kittypuppyboom/Assets/Scripts/SpeedPowerUp.cs
+++ kittypuppyboom/Assets/Scripts/SpeedPowerUp.cs
@@ -4,21 +4,32 @@ using System.Collections;
 public class SpeedPowerUp : MonoBehaviour {
 
 	private bool buff;
-	private bool currState;
-	private GameObject controller;
+	private Component controller;
 
 	// Use this for initialization
 	void Start () {
-		controller = GameObject.Find("GameController");
+		controller = GameObject.Find("GameController").GetComponents<GameController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(this.tag.Equals("catPowerUp") && currState != controller
+		if(this.tag.Equals("catPowerUp") && buff != controller.catState){
+			buff = controller.catState;
+		}else if(this.tag.Equals("dogPowerUp") && buff != controller.dogState){
+			buff = controller.dogState;
+		}
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-
+		if(this.tag.Equals("catPowerUp") && buff){
+			controller.SpeedPowerUp(0);
+		}else if (this.tag.Equals("catPowerUp")){
+			controller.SlowPowerUp(0);
+		}else if (buff){
+			controller.SpeedPowerUp(1);
+		} else {
+			controller.SlowPowerUp(1);
+		}
 	}
 }
